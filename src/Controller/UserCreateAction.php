a4a026cb6936cfd72e47d829fc6ee44a1f8228b5
Abuudev <abdulloh3525Dev@gmail.com>
2022-06-16 20:06:37 +0500

    add (UserFactory)

--- src/Controller/UserCreateAction.php
+++ src/Controller/UserCreateAction.php
@@ -4,11 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class UserCreateAction
+use App\Component\User\UserFactory;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserCreateAction extends AbstractController
 {
-    public  function  __invoke(): void
+
+    public function __construct(private UserFactory $userFactory)
     {
-        print 'Hello, World';
-        exit;
+    }
+
+    public function __invoke(User $data): void
+    {
+        $user = $this->userFactory->create($data->getEmail(), $data->getPassword());
+
+        print_r($user);
+
+        exit();
     }
 }
