df91e08d033f72ef7539d1b34739cdf257ab5824
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-10-07 10:43:44 +0500

    Manage read/write

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -5,27 +5,40 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    collectionOperations: [
+        'get', 'post'
+    ],
+    itemOperations: ['delete', 'get', 'put'],
+    denormalizationContext: ['groups' => ['book:write']],
+    normalizationContext: ['groups' => ['book:read']]
+)]
 class Book
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['user:read'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['book:read', 'book:write'])]
     private $name;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Groups(['book:read', 'book:write'])]
     private $description;
 
     #[ORM\Column(type: 'text')]
+    #[Groups(['book:read', 'book:write'])]
     private $text;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'books')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['book:read', 'book:write'])]
     private $category;
 
     public function getId(): ?int
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,17 +7,27 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    collectionOperations: [
+        'get', 'post'
+    ],
+    itemOperations: ['delete', 'get', 'put'],
+    denormalizationContext: ['groups' => ['category:write']],
+    normalizationContext: ['groups' => ['category:read']]
+)]
 class Category
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['category:read'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['category:read', 'category:write'])]
     private $name;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Book::class)]
